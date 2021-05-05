81c3bedf469e4ae2786c170751e0a9b1127e7a75
Ed94 <edwardgz@gmail.com>
2021-05-05 19:58:41 -0400

    Did initial unravel...

--- index.js
+++ index.js
@@ -1,283 +1,468 @@
 // Example express application adding the parse-dashboard module to expose Parse Dashboard compatible API routes.
-const https = require('https');
-const fs = require("fs");
 
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
+
+
+const https = require('https');
+const fs    = require("fs");
+
+
+var express        = require('express');
+var ParseServer    = require('parse-server').ParseServer;
 var ParseDashboard = require('parse-dashboard');
-var path = require('path');
-var nodemailer = require('nodemailer');
+var path           = require('path');
+var nodemailer     = require('nodemailer');
+
+
 
 require('dotenv').config();
 
-var api = new ParseServer({
+
+
+var api = new ParseServer
+({
     databaseURI: process.env.DATABASE_URI,
-    cloud: __dirname + '/cloud/main.js',
-    appId: process.env.APP_ID,
-    masterKey: process.env.MASTER_KEY,
-    serverURL: process.env.SERVER_URL,
-    appName: process.env.APP_NAME
+    cloud:       __dirname + '/cloud/main.js',
+    appId:       process.env.APP_ID,
+    masterKey:   process.env.MASTER_KEY,
+    serverURL:   process.env.SERVER_URL,
+    appName:     process.env.APP_NAME
 })
 
-var dashboard = new ParseDashboard({
-  apps: [
+var dashboard = new ParseDashboard
+({
+  apps: 
+  [
     {
-      appId: process.env.APP_ID,
+      appId:     process.env.APP_ID,
       masterKey: process.env.MASTER_KEY,
       serverURL: process.env.SERVER_URL,
-      appName: process.env.APP_NAME,
+      appName:   process.env.APP_NAME,
     },
   ],
-  users: [
+
+  users: 
+  [
     {
-      user: 'ScrutinyAdmin001',//process.env.USERNAME || 'test',
-      pass: '$2y$12$9Ut7yIlbT1WNMVrruBC0m.6Bgq8gkmZak8SE7pKrbQbTk1gzNkbVu'//process.env.PASSWORD || 'test'
+      user: 'ScrutinyAdmin001',                                             // process.env.USERNAME || 'test',
+      pass: '$2y$12$9Ut7yIlbT1WNMVrruBC0m.6Bgq8gkmZak8SE7pKrbQbTk1gzNkbVu'  // process.env.PASSWORD || 'test'
     }
   ],
+
   "useEncryptedPasswords": true
 });
 
-var transporter = nodemailer.createTransport({
+var transporter = nodemailer.createTransport
+({
   service: 'gmail',
-  auth: {
+
+  auth: 
+  {
     user: process.env.EMAIL_NAME,
     pass: process.env.EMAIL_PASS
   }
 });
 
 var app = express();
+
 //app.enable('trust proxy');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+app.use(
+function(req, res, next) 
+{
+  // Update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*");    
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
   next();
 });
 
-// make the Parse Dashboard available at /
+
+// Make the Parse Dashboard available at /
 app.use('/dashboard', dashboard);
 
 var mountPath = process.env.PARSE_MOUNT;
+
 app.use(mountPath, api);
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 
-app.get('/bonjour', function(req,res){
+app.get('/bonjour', 
+function(req, res)
+{
   console.log("[200] " + req.method + " to " + req.url);
+
   res.statusCode = 200
-  try{
+
+  try
+  {
     res.send("Bonjour! Je suis vivant!");
   }
-  catch(e){
+  catch(e)
+  {
     res.send(false);
   }
 });
 
-app.post('/feedback', function(req,res){
+app.post('/feedback', 
+function(req, res)
+{
   console.log("[200] " + req.method + " to " + req.url);
+
   res.statusCode = 200
 
   var body = "";
-  req.on('data', function(chunk) {
-      body += chunk;
-    }).on('end', async function() {
 
-      let mreq = JSON.parse(body)
-      var description = mreq.user  +" sent the following feedback:\n";
-      description += mreq.feedback;
+  req
+  .on('data', 
+  function(chunk) 
+  {
+    body += chunk;
+  })
+  .on('end', 
+  async function() 
+  {
+    let mreq = JSON.parse(body)
 
-      var mailOptions = {
-        from: process.env.EMAIL_NAME,
-        to: process.env.EMAIL_NAME,
-        subject: 'Feedback from: ' + mreq.user,
-        text: description
-      };
+    var description = mreq.user  +" sent the following feedback:\n";
 
-      try{
-        transporter.sendMail(mailOptions, function(error, info){
-          if (error) {
-            console.log(error);
-            res.send(null);
-          } else {
-            res.send('Email sent: ' + info.response);
-            console.log('Email sent: ' + info.response);
-          }
-        });
+    description += mreq.feedback;
+
+    var mailOptions = 
+    {
+      from:     process.env.EMAIL_NAME,
+      to:       process.env.EMAIL_NAME,
+      subject: 'Feedback from: ' + mreq.user,
+      text:     description
+    };
+
+    try
+    {
+      transporter
+      .sendMail(mailOptions, 
+      function(error, info)
+      {
+        if (error) 
+        {
+          console.log(error);
+          
+          res.send(null);
+        } 
+        else 
+        {
+          res.send('Email sent: ' + info.response);
+
+          console.log('Email sent: ' + info.response);
         }
-      catch(e){
-        res.send(null);
-        }
-    });
+      });
+    }
+    catch(e)
+    {
+      res.send(null);
+    }
+  });
 });
 
-app.post('/modelrequest', function(req,res){
+app.post('/modelrequest', 
+function(req, res)
+{
   console.log("[200] " + req.method + " to " + req.url);
+
   res.statusCode = 200
 
   var body = "";
-  req.on('data', function(chunk) {
-      body += chunk;
-    }).on('end', async function() {
 
+  req
+  .on('data', 
+  function(chunk) 
+  {
+    body += chunk;
+  })
+  .on('end', 
+  async function() 
+  {
       let mreq = JSON.parse(body)
-      var model = mreq.model;
+
+      var model       = mreq.model;
       var description = mreq.user  +" requested a : " + model +"\n\n";
+
       description += "Description: \n";
       description += mreq.description
 
-      var mailOptions = {
-        from: process.env.EMAIL_NAME,
-        to: process.env.EMAIL_NAME,
+      var mailOptions = 
+      {
+        from:    process.env.EMAIL_NAME,
+        to:      process.env.EMAIL_NAME,
         subject: 'Model Request: ' + model,
-        text: description
+        text:    description
       };
 
-      try{
-        transporter.sendMail(mailOptions, function(error, info){
-          if (error) {
+      try
+      {
+        transporter.sendMail(mailOptions, 
+
+        function(error, info)
+        {
+          if (error) 
+          {
             console.log(error);
+
             res.send(null);
-          } else {
+          } 
+          else 
+          {
             res.send('Email sent: ' + info.response);
+
             console.log('Email sent: ' + info.response);
           }
         });
-        }
-      catch(e){
+      }
+      catch(e)
+      {
         res.send(null);
-        }
+      }
     });
 });
 
-//Request
-app.post('/login', function(req,res){
+// Request
+app.post('/login', 
+function(req, res)
+{
   console.log("[200] " + req.method + " to " + req.url);
+
   res.statusCode = 200
 
   var body = "";
-  req.on('data', function(chunk) {
-      body += chunk;
-    }).on('end', async function() {
 
-    try{
+  req
+  .on('data', 
+  function(chunk) 
+  {
+      body += chunk;
+  })
+  .on('end', 
+  async function() 
+  {
+
+    try
+    {
       let jsonUSer = JSON.parse(body)
       let username = jsonUSer.user
-      let pwd = jsonUSer.pwd
+      let pwd      = jsonUSer.pwd
 
       //console.log(username);
       //console.log(pwd);
 
       var user = Parse.User
-              .logIn(username, pwd).then(function(user) {
-                  console.log('User created successful with name: ' + user.get("username") + ' and email: ' + user.get("email"));
-                  let userInfo = user.id+"|"+user.get("email");
-                  res.send(userInfo);
-          }).catch(function(error){
-              console.log("Error: " + error.code + " " + error.message);
-              res.send("invalid");
-          });
-    } catch (e) { res.send("timeout"); }
+      .logIn(username, pwd)
+      .then(
+      function(user) 
+      {
+        console.log('User created successful with name: ' + user.get("username") + ' and email: ' + user.get("email"));
+
+        let userInfo = user.id+"|"+user.get("email");
+
+        res.send(userInfo);
+      })
+      .catch(
+      function(error)
+      {
+        console.log("Error: " + error.code + " " + error.message);
+        res.send("invalid");
+      });
+    } 
+    catch (e) 
+    { 
+      res.send("timeout"); 
+    }
   });
 });
 
-app.post('/loadscene', function(req,res){
+app.post('/loadscene', 
+function(req, res)
+{
   console.log("[200] " + req.method + " to " + req.url);
+
   res.statusCode = 200
 
   var body = "";
-  req.on('data', function(chunk) {
-      body += chunk;
-    }).on('end', async function() {
 
-    try{
+  req
+  .on('data', 
+  function(chunk) 
+  {
+    body += chunk;
+  })
+  .on('end', 
+  async function() 
+  {
+    try
+    {
       let jsonSceneId = JSON.parse(body)
 
       console.log(jsonSceneId.id);
 
-      Parse.Cloud.run('getSceneFromId', {jsonSceneId}).then((scenes) => {
+      Parse.Cloud
+      .run('getSceneFromId', {jsonSceneId})
+      .then((scenes) => 
+      {
         let scene = scenes[0];
+
         console.log(scene.get("json"));
+
         res.send(scene.get("json"));
-      }).catch(console.log)
-    } catch (e) { res.send(false) }
+
+      })
+      .catch(console.log)
+    } 
+    catch (e) 
+    { 
+      res.send(false) 
+    }
   });
 });
 
-app.post('/loadscenes', function(req,res){
+app.post('/loadscenes', 
+function(req, res)
+{
   console.log("[200] " + req.method + " to " + req.url);
+
   res.statusCode = 200
 
   var body = "";
-  req.on('data', function(chunk) {
-      body += chunk;
-    }).on('end', async function() {
 
-    try{
+  req
+  .on('data', 
+  function(chunk) 
+  {
+      body += chunk;
+  })
+  .on('end', 
+  async function() 
+  {
+    try
+    {
       let jsonUserId = JSON.parse(body)
 
-      Parse.Cloud.run('getScenesOfUser', {jsonUserId}).then((scenes) => {
+      Parse.Cloud
+      .run('getScenesOfUser', {jsonUserId})
+      .then((scenes) => 
+      {
         let jsonscenes = []
-        for(var a in scenes){
+
+        for(var a in scenes)
+        {
           let scene = scenes[a]
+
           jsonscenes.push(JSON.parse(scene.get("json")));
         }
+
         //console.log(jsonscenes[0]);
+        
         res.send(jsonscenes);
-      }).catch(console.log)
-    } catch (e) { res.send(false) }
+
+      })
+      .catch(console.log)
+    } 
+    catch (e) 
+    { 
+      res.send(false) 
+    }
   });
 });
 
-app.post('/savescene', function(req,res){
+app.post('/savescene', 
+function(req,res)
+{
   console.log("[200] " + req.method + " to " + req.url);
+
   res.statusCode = 200
 
   var body = "";
-  req.on('data', function(chunk) {
-      body += chunk;
-    }).on('end', async function() {
 
-    try{
+  req
+  .on('data', 
+  function(chunk) 
+  {
+      body += chunk;
+  })
+  .on('end', 
+  async function() 
+  {
+    try
+    {
       let savedScene = JSON.parse(body)
 
-
-      Parse.Cloud.run('saveScene', {savedScene}).then((savedSceneId) => {
+      Parse.Cloud
+      .run('saveScene', {savedScene})
+      .then((savedSceneId) => 
+      {
         console.log(savedSceneId);
+
         res.send(savedSceneId);
-      }).catch(function(error){
+      })
+      .catch(
+      function(error)
+      {
           console.log("Error: " + error.code + " " + error.message);
+
           res.send("error");
       });
-    } catch (e) { res.send(false) }
+    } 
+    catch (e) 
+    { 
+      res.send(false) 
+    }
   });
 });
 
-app.post('/deletescene', function(req,res){
+app.post('/deletescene', 
+function(req, res)
+{
   console.log("[200] " + req.method + " to " + req.url);
+
   res.statusCode = 200
 
   var body = "";
-  req.on('data', function(chunk) {
-      body += chunk;
-    }).on('end', async function() {
 
-    try{
+  req
+  .on('data', 
+  function(chunk) 
+  {
+    body += chunk;
+  })
+  .on('end', 
+  async function() 
+  {
+    try
+    {
       let deletedScene = JSON.parse(body)
 
-
-      Parse.Cloud.run('deleteSceneFromId', {deletedScene}).then((deletedSceneId) => {
+      Parse.Cloud
+      .run('deleteSceneFromId', {deletedScene})
+      .then((deletedSceneId) => 
+      {
         console.log("Delete " + deletedSceneId);
+
         res.send(deletedSceneId.id);
-      }).catch(function(error){
+      })
+      .catch(
+      function(error)
+      {
           console.log("Error: " + error.code + " " + error.message);
+
           res.send("error");
       });
-    } catch (e) { res.send(false) }
+    } 
+    catch (e) 
+    { 
+      res.send(false) 
+    }
   });
 });
 
 
 var port = process.env.PORT;
+
 /*
 var options = {
     key: fs.readFileSync('./keys/privkey.pem'),
@@ -291,6 +476,10 @@ httpsServer.listen(port, function() {
 */
 
 var httpServer = require('http').createServer(app);
-httpServer.listen(port, function() {
+
+
+httpServer.listen(port, 
+function() 
+{
   console.log('HTTP: Scrutiny server running on port ' + port + '.');
 });
